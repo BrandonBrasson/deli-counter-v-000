89172384eb0f97ecf944fa30d5774e056adcb834
Brandon Brasson <rocksong622@gmail.com>
2019-01-10 18:29:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli << name
+  return katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
@@ -24,6 +24,6 @@ def now_serving(katz_deli)
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli.first}."
-    katz_deli.shift
+    return katz_deli.shift
   end
 end
